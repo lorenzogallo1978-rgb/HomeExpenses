a23d22f56fb13ba6e544f75411bb8bfd346d3710
lorenzogallo1978-rgb <lorenzogallo1978@gmail.com>
2026-09-20 21:57:51 +0300

    Create TransactionDao.kt

---
+++ app/src/main/java/com/homebudget/expenses/data/TransactionDao.kt
@@ -0,0 +1,20 @@
+package com.homebudget.expenses.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TransactionDao {
+    @Query("SELECT * FROM transactions ORDER BY date DESC")
+    fun getAllTransactions(): Flow<List<Transaction>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(transaction: Transaction)
+
+    @Delete
+    suspend fun delete(transaction: Transaction)
+}
